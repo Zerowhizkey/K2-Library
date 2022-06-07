da3a7ba57cfc5819cd5ec76e122f74b070974098
Marcus Piri <Marcus.piri@cmeducations.se>
2022-06-07 15:02:48 +0200

    Fixed config/db.js file

---
+++ config/db.js
@@ -0,0 +1,27 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./db.sqlite", (error) => {
+	if (error) {
+		console.error(error.message);
+		throw error;
+	}
+
+	const booksStmt = `
+    CREATE TABLE IF NOT EXISTS books (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        author TEXT,
+        published TEXT,
+        pages TEXT,
+    )
+    `;
+
+	db.run(booksStmt, (error) => {
+		if (error) {
+			console.error(error.message);
+			// throw error;
+		}
+	});
+});
+
+module.exports = db;
