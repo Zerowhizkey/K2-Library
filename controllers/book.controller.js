6e6d9c0d5b2239c5af882074f1a9ff5df87a6484
Marcus Piri <Marcus.piri@cmeducations.se>
2022-06-09 11:05:34 +0200

    fixed to a more serius error/status messages

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -3,25 +3,25 @@ const model = require("../models/book");
 async function getAllBooks(req, res) {
 	const books = await model.getAll();
 	if (!books) {
-		return res.status(400).json({ message: "buhu u suckie" });
+		return res.status(400).json({ error: "Cannot get Books" });
 	}
 
-	res.status(200).json({ message: "goodie", books });
+	res.status(200).json({ status: "Request successfull", books });
 }
 
 async function getSingleBooks(req, res) {
 	const book = await model.getSingle(req.params.id);
 	if (!req.params.id || !book) {
-		return res.status(400).json({ message: "suckie suckie againi" });
+		return res.status(404).json({ error: "Book not found" });
 	}
-	res.status(200).json({ message: "woho goodie", book });
+	res.status(200).json({ status: "Request successfull", book });
 }
 
 async function postBook(req, res) {
 	const { title, author, pages } = req.body;
 
 	if (!title || !author || !pages) {
-		res.status(400).json({ message: "You got to send all Input" });
+		res.status(400).json({ error: "Bad request" });
 		return;
 	}
 
@@ -33,20 +33,24 @@ async function postBook(req, res) {
 
 	const result = await model.addOne(newBook);
 
-	res.status(201).json({ message: "yay", id: result.lastID, ...newBook });
+	res.status(201).json({
+		status: "Successfully created",
+		id: result.lastID,
+		...newBook,
+	});
 }
 
 async function removeSingleBook(req, res) {
 	const result = await model.deleteOne(req.params.id);
 	if (result.changes === 0) {
-		return res.status(400).json({ message: "bad request" });
+		return res.status(400).json({ error: "Bad request" });
 	}
-	res.status(200).json({ message: "succesfully deleted!" });
+	res.status(200).json({ status: "Succesfully removed" });
 }
 
 async function fullEditBook(req, res) {
 	if (!req.body.title || !req.body.author || !req.body.pages) {
-		res.status(400).json({ message: "u suckie" });
+		res.status(400).json({ error: "Missing data" });
 		return;
 	}
 	model.fullEdit(
@@ -58,26 +62,26 @@ async function fullEditBook(req, res) {
 	const result = await model.getSingle(req.params.id);
 	console.log(result);
 	if (!result) {
-		res.status(400).json({ message: "fuck off! u suckie" });
+		res.status(404).json({ error: "Could not find" });
 		return;
 	}
 	console.log(result);
-	res.status(200).json({ message: "u goodie!", updated: req.body });
+	res.status(200).json({ status: "Succesfully edited", updated: req.body });
 }
 
 async function editBook(req, res) {
 	if (!req.body.title && !req.body.author && !req.body.pages) {
-		res.status(400).json({ message: "u suckie" });
+		res.status(400).json({ error: "Bad request" });
 		return;
 	}
 	model.edit(req.params.id, req.body.title, req.body.author, req.body.pages);
 	const result = await model.getSingle(req.params.id);
 	if (!result) {
-		res.status(400).json({ message: "fuck off! u suckie" });
+		res.status(400).json({ error: "Could not find" });
 		return;
 	}
 	console.log(result);
-	res.status(200).json({ message: "u goodie!", updated: req.body });
+	res.status(200).json({ status: "Succesfully edited", updated: req.body });
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.json());
 
 app.use("/books", bookRouter);
 
+app.use((error, req, res, next) => {
+	console.error(error.stack);
+	res.status(500).json({ error: "Internal Server Error" });
+});
+
 app.listen(4000, () => {
 	console.log("Servern körs på port 4000");
 });
